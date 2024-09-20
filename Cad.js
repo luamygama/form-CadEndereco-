349b2861374bfac68de72534f47e2d48c2843f1a
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-09-20 11:03:11 -0300

    ativando o modo restrito

---
+++ Cad.js
@@ -0,0 +1,8 @@
+'use strict'; //Ativa o modo restrito 
+// Esse modo faz com que o javascript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação.
+/* Consumo de API - https://viacep.com.br/*/
+
+//Função para limpar campos preenchidos
+const limparformulario = () => {
+
+}
