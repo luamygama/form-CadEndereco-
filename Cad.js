0c7c4565f5c595029763f184c7b3774b090af180
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-09-20 11:30:21 -0300

    adiconando função para identificar quantos numeros foram digitados e se são igual a 8 digito

--- Cad.js
+++ Cad.js
@@ -9,3 +9,6 @@ const limparformulario = () => {
     document.getElementById('bairro').value = '';
     document.getElementById('uf').value = '';
 }
+// verifica se o CEP e valido
+const eNumero = (numero) => /^[0-9]+$/.test(numero);   // combinação de numero de inico e fim 
+const cepValido = (cep) => cep.length == 8 && eNumero(cep)             //Identifica quantos caracteres foram digitados corespondente aos 8 digitos
